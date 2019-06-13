637f8c26447fa73e88c409297b5222ede7afd789
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-06-13 07:39:13 +0100

    first related schema created

--- db/migrate/20190613053438_create_org_projects.rb
+++ db/migrate/20190613053438_create_org_projects.rb
@@ -1,8 +1,8 @@
 class CreateOrgProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :org_projects do |t|
-      t.references :project_id, foreign_key: true
-      t.references :org_id, foreign_key: true
+      t.references :project, foreign_key: true
+      t.references :org, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20190613053538_create_user_org_projects.rb
+++ db/migrate/20190613053538_create_user_org_projects.rb
@@ -3,8 +3,8 @@ class CreateUserOrgProjects < ActiveRecord::Migration[5.2]
     create_table :user_org_projects do |t|
       t.datetime :start_date
       t.datetime :end_date
-      t.references :user_id, foreign_key: true
-      t.references :org_projects, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :org_project, foreign_key: true
 
       t.timestamps
     end
