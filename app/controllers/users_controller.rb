8293665c0150e4e4f4e7ee98965ea199d3c6ef1d
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-06-19 15:01:13 +0100

    added in all params to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:first_name, :last_name, :username, :email)
   end
 
   def update_params
