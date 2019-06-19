a9d64b8bbbbecce6065da9c259ba188bb435c1e5
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-06-19 07:46:14 +0100

    user policies and user show route and show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
-  # render json: @user
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def show
+    authorize @user
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    # params.require(:user).permit()
+  end
+
+  def update_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,25 @@
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      if user.admin
+        scope.all
+      end
     end
+  end
+
+  def show?
+    true
+  end
+
+  def edit?
+    record == user
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    user.admin
   end
 end
