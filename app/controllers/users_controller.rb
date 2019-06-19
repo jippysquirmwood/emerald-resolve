febf66cfbf9d4e84494acf81c33b7537238c1995
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-06-19 14:58:09 +0100

    new_user signup fields put into the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,25 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def new
+    @user = User.new
+    authorize @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    authorize @user
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
   private
 
   def user_params
-    # params.require(:user).permit()
+    params.require(:user).permit(:email)
   end
 
   def update_params
